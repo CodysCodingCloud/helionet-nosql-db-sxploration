c12ceb6b9661a489f77f032c29d042d610e89ba1
alejandra100501ar <155079050+alejandra100501ar@users.noreply.github.com>
2026-03-16 15:40:08 -0400

    Added redis config for docker

--- src/RedisInteraction.py
+++ src/RedisInteraction.py
@@ -1,40 +1,73 @@
-from src.hetionetDBInteraction import hetionetDBInteraction
-
+from hetionetDBInteraction import hetionetDBInteraction
 import redis
 from dotenv import load_dotenv
+import os
+import csv
 
-r = redis.Redis(
-    host = os.getenv('REDIS_HOST')
-    port = int(os.getenv('REDIS_PORT'))
-    password = os.getenv('REDIS_PASSWORD')
-    decode_responses = true
-)
-
-response = r.ping()
-
-try
-if response:
-    print ("Success")
-
-except Exception as e:
-    print("Something was down :( check it out: {e}" )
 
 
 class RedisInteraction(hetionetDBInteraction):
-    def db_conn_service():
+    def db_conn_service(self):
+        load_dotenv()
+
+        self.r = redis.Redis(
+            host = os.getenv('REDIS_HOST'),
+            port = int(os.getenv('REDIS_PORT')),
+            password = os.getenv('REDIS_PASSWORD'),
+            decode_responses = True
+        )
+
+        response = self.r.ping()
+
+        try:
+            if response:
+                print ("Success")
+        except Exception as e:
+            print(f"Something was down :( check it out: {e}" )
+            
+    def populate_db(self, nodes_path, edge_path):
+        
+        with open(nodes_path, 'r') as f:
+            data = csv.DictReader(f, delimiter ='\t')
+            for row in data:
+                node_id = row['id']
+                self.r.hset(f"node:{node_id}", mapping=row)
+
+        with open(edge_path, 'r') as f:
+            data = csv.DictReader(f, delimiter = '\t')
+            for row in data:
+                source = row['source']
+                rel = row['metaedge']
+                target = row['target']
+                
+                self.r.sadd(f"links:{target}:{rel}", source)
+                self.r.sadd(f"links:{source}:{rel}", target)
+
+
+    def get_all_diseases(self):
         pass
-    def populate_db():
-        pass
-    def get_all_diseases():
-        pass
-    def get_disease_by_id(disease_id)->dict:
+        
+    def get_disease_by_id(self, disease_id):
         """
         Given a disease id, what is its name, what are drug names that can treat or palliate this disease, what are gene names that cause this disease, and where this disease occurs? Obtain and output this information in a single query.
         """
-        pass
-    def get_disease_drug_interactions_by_id(disease_id):
-        """
-        We assume that a compound can treat a disease if the compound up-regulates/down-regulates a gene, but the location down-regulates/up-regulates the gene in an opposite direction where the disease occurs. Find all compounds that can treat a new disease (i.e. the missing edges between compound and disease excluding existing drugs). Obtain and output all drugs in a single query.
-        """
-        pass
+        batch = self.r.pipeline()
+        
+        batch.hgetall(f"node: {disease_id}")
+
+        batch.smembers(f"links: {disease_id}:CtD") 
+        batch.smembers(f"links: {disease_id}:CpD")
+        batch.smembers(f"links: {disease_id}:DaG")
+        batch.smembers(f"links: {disease_id}:DlA")
+
+        results = batch.execute()
+
+        return {
+                "disease_name": results[1].get("name"),
+                "compounds that treat": list(results[1]),
+                "compounds that palliate": list(results[2]),
+                "associated genes": list(results[3]),
+                "locations": list(results[4])
+            }
+
         
